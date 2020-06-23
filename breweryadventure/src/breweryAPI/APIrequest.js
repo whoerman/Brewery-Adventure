3643a0dbb54254eb47f3c2c25d6f6db485a8480d
Walter Hoerman <whoerman@gmail.com>
2020-06-23 09:03:03 -0400

    saving Postman result

--- breweryadventure/src/breweryAPI/APIrequest.js
+++ breweryadventure/src/breweryAPI/APIrequest.js
@@ -1,7 +1,15 @@
-let state = "ohio"
+// import axios from "axios";
 
-axios.get(`https://api.openbrewerydb.org/breweries?by_state=${state}`)
-  .then((response) => {
-    let currentBreweries = response.data;
-    console.log(currentBreweries);
-  });
+// function getBreweries() {
+
+//     let state = "ohio"
+
+//     axios.get(`https://api.openbrewerydb.org/breweries?by_state=${state}`)
+//       .then((response) => {
+//         let currentBreweries = response.data;
+//         console.log(currentBreweries);
+//       });
+//       return getBreweries
+// }
+
+
