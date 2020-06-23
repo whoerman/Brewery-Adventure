7db2ac05e17fc176c70a5c795ae26aae329d615f
Walter Hoerman <whoerman@gmail.com>
2020-06-23 07:38:04 -0400

    testing axios to database

--- breweryadventure/src/App.js
+++ breweryadventure/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import './App.css';
+import axios from "axios"
 
 function App() {
+
+  let state ="ohio"
+
+  axios.get(`https://api.openbrewerydb.org/breweries?by_state=${state}`)
+  .then((response) => {
+    let currentBreweries = response.data;
+    console.log(currentBreweries);
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
---
+++ breweryadventure/src/breweryAPI/APIrequest.js
@@ -0,0 +1,7 @@
+let state = "ohio"
+
+axios.get(`https://api.openbrewerydb.org/breweries?by_state=${state}`)
+  .then((response) => {
+    let currentBreweries = response.data;
+    console.log(currentBreweries);
+  });
