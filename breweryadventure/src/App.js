4776762e205ddb236d97617831d5c098a0b6f1cb
Walter Hoerman <whoerman@gmail.com>
2020-06-24 19:36:26 -0400

    add USA Create React Map

--- breweryadventure/src/App.js
+++ breweryadventure/src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
+// import React from 'react';
+import Container from 'react-bootstrap/Container'
 import './App.css';
 import breweries from "../src/breweryAPI/brewerytest";
 
-function App() {
+// function App() {
 
-  console.log(breweries)
+//   console.log(breweries)
   
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Brewery Adventure
-        </p>
-      </header>
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Brewery Adventure
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
 
+// export default App;
+
+
+import React, { Component } from 'react';
+import USAMap from "react-usa-map";
+
+ 
+class App extends Component {
+  /* mandatory */
+  mapHandler = (event) => {
+    alert(event.target.dataset.name);
+  };
+ 
+  render() {
+    return (
+      <div className="App">
+        <Container>
+        <USAMap onClick={this.mapHandler} />
+        </Container>
+      </div>
+    );
+  }
+}
+ 
 export default App;
