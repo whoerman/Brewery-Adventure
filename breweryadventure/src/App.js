600ac9f7cbe5fa0cba05d40b205570b7a3324c5e
Walter Hoerman <whoerman@gmail.com>
2020-06-24 20:28:20 -0400

    adding map colors

--- breweryadventure/src/App.js
+++ breweryadventure/src/App.js
@@ -1,41 +1,32 @@
 // import React from 'react';
-import Container from 'react-bootstrap/Container'
 import './App.css';
 import breweries from "../src/breweryAPI/brewerytest";
-
-// function App() {
-
-//   console.log(breweries)
-  
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Brewery Adventure
-//         </p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+import StatesObj from "../src/MapSetup/InitialStates";
 
 
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import USAMap from "react-usa-map";
 
  
 class App extends Component {
+
   /* mandatory */
   mapHandler = (event) => {
-    alert(event.target.dataset.name);
-  };
+    let stateAbbrev = event.target.dataset.name;
+    console.log(stateAbbrev);
+  }
  
   render() {
     return (
-      <div className="App">
-        <Container>
-        <USAMap onClick={this.mapHandler} />
+      <div classname="App">
+        <Container fluid>
+          <Row>
+            <Col sm={8}><USAMap onClick={this.mapHandler} customize={StatesObj} /></Col>
+            <Col sm={4}><p>Bacon ipsum dolor amet hamburger pork ham, shankle sausage tail andouille pig venison porchetta spare ribs biltong frankfurter pastrami. Leberkas pig picanha sausage prosciutto, swine ham turducken ground round kevin alcatra chislic bacon. Buffalo pork chop ham leberkas. </p></Col>
+          </Row>
         </Container>
       </div>
     );
---
+++ breweryadventure/src/MapSetup/InitialStates.js
@@ -0,0 +1,305 @@
+    const StatesObj = {
+        "AK": {
+          fill: "#edae2a",
+          fullName: "Alaska",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "AL": {
+          fill: "#edae2a",
+          fullName: "Alabama",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "AR": {
+          fill: "#ac241a",
+          fullName: "Arkansas",
+          clicked: false,
+          originalFill: "#ac241a"
+        },
+        "AZ": {
+          fill: "#fefb71",
+          fullName: "Arizona",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "CA": {
+          fill: "#fefb71",
+          fullName: "California",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "CO": {
+          fill: "#691e21",
+          fullName: "Colorado",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "CT": {
+          fill: "#ac241a",
+          fullName: "Connecticut",
+          clicked: false,
+          originalFill: "#ac241a"
+        },
+        "DE": {
+          fill: "#edae2a",
+          fullName: "Deleware",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "FL": {
+          fill: "#fefb71",
+          fullName: "Florida",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "GA": {
+          fill: "#691e21",
+          fullName: "Georgia",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "HI": {
+          fill: "#edae2a",
+          fullName: "Hawaii",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "IA": {
+          fill: "#edae2a",
+          fullName: "Iowa",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "ID": {
+          fill: "#691e21",
+          fullName: "Idaho",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "IL": {
+          fill: "#fefb71",
+          fullName: "Illinois",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "IN": {
+          fill: "#ac241a",
+          fullName: "Indiana",
+          clicked: false,
+          originalFill: "#ac241a"
+        },
+        "KS": {
+          fill: "#edae2a",
+          fullName: "Kansas",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "KY": {
+          fill: "#edae2a",
+          fullName: "Kentucky",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "LA": {
+          fill: "#691e21",
+          fullName: "Louisiana",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "MA": {
+          fill: "#691e21",
+          fullName: "Massachussetts",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "MD": {
+          fill: "#fefb71",
+          fullName: "Maryland",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "ME": {
+          fill: "#691e21",
+          fullName: "Maine",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "MI": {
+          fill: "#691e21",
+          fullName: "Michigan",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "MN": {
+          fill: "#691e21",
+          fullName: "Minnesota",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "MO": {
+          fill: "#691e21",
+          fullName: "Missouri",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "MS": {
+          fill: "#edae2a",
+          fullName: "Mississippi",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "MT": {
+          fill: "#fefb71",
+          fullName: "Montana",
+          clicked: false,
+          originalFill:  "#fefb71"
+        },
+        "NC": {
+          fill: "#ac241a",
+          fullName: "North Carolina",
+          clicked: false,
+          originalFill: "#ac241a"
+        },
+        "ND": {
+          fill: "#edae2a",
+          fullName: "North Dakota",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "NE": {
+          fill: "#fefb71",
+          fullName: "Nebraska",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "NH": {
+          fill: "#fefb71",
+          fullName: "New Hampshire",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "NJ": {
+          fill: "#edae2a",
+          fullName: "New Jersey",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "NM": {
+          fill: "#edae2a",
+          fullName: "New Mexico",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "NV": {
+          fill: "#edae2a",
+          fullName: "Nevada",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "NY": {
+          fill: "#fefb71",
+          fullName: "New York",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "OH": {
+          fill: "#fefb71",
+          fullName: "Ohio",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "OK": {
+          fill: "#ac241a",
+          fullName: "Oklahoma",
+          clicked: false,
+          originalFill: "#ac241a"
+        },
+        "OR": {
+          fill: "#ac241a",
+          fullName: "Oregon",
+          clicked: false,
+          originalFill:  "#ac241a"
+        },
+        "PA": {
+          fill: "#691e21",
+          fullName: "Pennsylvania",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "RI": {
+          fill: "#fefb71",
+          fullName: "Rhode Island",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "SC": {
+          fill: "#edae2a",
+          fullName: "South Carolina",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "SD": {
+          fill: "#ac241a",
+          fullName: "South Dakota",
+          clicked: false,
+          originalFill: "#ac241a"
+        },
+        "TN": {
+          fill: "#fefb71",
+          fullName: "Tennessee",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "TX": {
+          fill: "#fefb71",
+          fullName: "Texas",
+          clicked: false,
+          originalFill: "#fefb71"
+        },
+        "UT": {
+          fill: "#ac241a",
+          fullName: "Utah",
+          clicked: false,
+          originalFill: "#ac241a"
+        },
+        "VA": {
+          fill: "#691e21",
+          fullName: "Virginia",
+          clicked: false,
+          originalFill: "#691e21"
+        },
+        "VT": {
+          fill: "#ac241a",
+          fullName: "Vermont",
+          clicked: false,
+          originalFill: "#ac241a"
+        },
+        "WA": {
+          fill: "#edae2a",
+          fullName: "Washington",
+          clicked: false,
+          originalFill: "#edae2a"
+        },
+        "WI": {
+          fill: "#ac241a",
+          fullName: "Wisconsin",
+          clicked: false,
+          originalFill: "#ac241a"
+        },
+        "WV": {
+          fill: "#ac241a",
+          fullName: "West Viginia",
+          clicked: false,
+          originalFill: "#ac241a"
+        },
+        "WY": {
+          fill: "#edae2a",
+          fullName: "Wyoming",
+          clicked: false,
+          originalFill: "#edae2a"
+        }
+    
+    };
+
+    export default StatesObj
